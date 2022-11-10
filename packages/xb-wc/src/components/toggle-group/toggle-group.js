522cba9cd555fbd81b27e993d57d0138aad19a2e
Welington Silva <welington.silva@loadsmart.com>
2022-11-10 12:14:10 -0300

    fix: replace selection controller with mixin

--- packages/xb-wc/src/components/toggle-group/toggle-group.js
+++ packages/xb-wc/src/components/toggle-group/toggle-group.js
@@ -2,17 +2,14 @@ import { html } from 'lit';
 import withClassy from '@welingtonms/classy';
 
 import { TOGGLE_EVENT } from './toggle-group.constants';
-import SelectionController from '../../controllers/selection';
+import SelectionManagerMixin from '../../mixins/selection';
 import styles from './toggle-group.styles';
 import XBElement from '../../common/xb-element';
 
 import '../layout/cluster';
 
-export class ToggleGroup extends XBElement {
+export class ToggleGroup extends SelectionManagerMixin( XBElement, TOGGLE_EVENT ) {
 	static styles = [ styles() ];
-
-	/** @type {SelectionController} */
-	selectionController;
 
 	/** @type {HTMLSlotElement} */
 	defaultSlot;
@@ -20,22 +17,24 @@ export class ToggleGroup extends XBElement {
 	static get properties() {
 		return {
 			/**
-			 * Strategy for toggling.
-			 * @type {import('@welingtonms/xb-toolset/dist/selection').SelectionType}
+			 * Should the button be disabled.
+			 * @type {ToggleGroupAttributes['disabled']}
 			 */
-			type: { type: String },
+			disabled: { type: Boolean, reflect: true },
+
+			/**
+			 * Button size.
+			 * @type {ToggleGroupAttributes['size']}
+			 */
+			size: { type: String },
 
 			/**
 			 * Aria role
-			 * @type {'group' | 'radiogroup'}
+			 * @type {ToggleGroupAttributes['role']}
 			 */
 			role: {
 				type: String,
 				reflect: true,
-			},
-
-			_selection: {
-				state: true,
 			},
 		};
 	}
@@ -43,35 +42,35 @@ export class ToggleGroup extends XBElement {
 	constructor() {
 		super();
 
+		/** @type {ToggleGroupAttributes['type']} */
 		this.type = 'single-strict';
-		this.role = [ 'single', 'single-strict' ].includes( this.type )
-			? 'radiogroup'
-			: 'group';
 
-		/** @type {import('@welingtonms/xb-toolset/dist/selection').SelectionState} */
-		this._selection = new Set();
+		this.role = [ 'single', 'single-strict' ].includes( this.type ) ? 'radiogroup' : 'group';
 	}
 
-	connectedCallback() {
-		super.connectedCallback();
+	/**
+	 * @param {import('lit').PropertyValues<ToggleGroup>} changedProperties
+	 */
+	updated( changedProperties ) {
+		super.updated( changedProperties );
 
-		this.selectionController = new SelectionController(
-			this,
-			this.type,
-			TOGGLE_EVENT
-		);
-	}
+		if ( changedProperties.has( 'role' ) ) {
+			this._getToggles().forEach( ( toggle ) => {
+				this._setToggleRole( toggle );
+			} );
+		}
 
-	firstUpdated() {
-		this.defaultSlot = this.renderRoot?.querySelector( 'slot' );
+		if ( changedProperties.has( 'disabled' ) ) {
+			this._getToggles().forEach( ( toggle ) => {
+				this._setToggleDisabled( toggle );
+			} );
+		}
 
-		this._handleSlotChange();
-	}
-
-	updated() {
-		this._getToggles().forEach( ( toggle ) => {
-			toggle.checked = this._selection.has( toggle.value );
-		} );
+		if ( changedProperties.has( '_selection' ) ) {
+			this._getToggles().forEach( ( toggle ) => {
+				this._setToggleChecked( toggle );
+			} );
+		}
 	}
 
 	render() {
@@ -88,26 +87,48 @@ export class ToggleGroup extends XBElement {
 				paddingless="none"
 				?disabled="${ this.disabled }"
 			>
-				<slot @slotchange=${ this._handleSlotChange }></slot>
+				<slot></slot>
 			</xb-cluster>
 		`;
 	}
 
-	_handleSlotChange() {
-		this._getToggles().forEach( ( toggle ) => {
-			toggle.setAttribute(
-				'role',
-				[ 'single', 'single-strict' ].includes( this.type )
-					? 'radio'
-					: 'checkbox'
-			);
-		} );
-	}
-
+	/**
+	 * @returns {import('./toggle').ToggleButton[]}
+	 */
 	_getToggles() {
+		this.defaultSlot = this.defaultSlot ?? this.shadowRoot.querySelector( 'slot' );
+
 		return [ ...this.defaultSlot.assignedElements( { flatten: true } ) ].filter(
 			( item ) => item.tagName.toLowerCase() === 'xb-toggle'
 		);
+	}
+
+	/**
+	 * @param {import('./toggle').ToggleButton} toggle
+	 */
+	_setToggleRole( toggle ) {
+		toggle.setAttribute(
+			'role',
+			[ 'single', 'single-strict' ].includes( this.type ) ? 'radio' : 'checkbox'
+		);
+	}
+
+	/**
+	 * @param {import('./toggle').ToggleButton} toggle
+	 */
+	_setToggleChecked( toggle ) {
+		if ( toggle.checked !== this._selection.has( toggle.value ) ) {
+			toggle.checked = this._selection.has( toggle.value );
+		}
+	}
+
+	/**
+	 * @param {import('./toggle').ToggleButton} toggle
+	 */
+	_setToggleDisabled( toggle ) {
+		if ( toggle.disabled !== this.disabled ) {
+			toggle.disabled = this.disabled;
+		}
 	}
 }
 
@@ -123,4 +144,18 @@ window.customElements.define( 'xb-toggle-group', ToggleGroup );
 /**
  * @typedef {('text' | 'ghost' | 'flat')} ButtonEmphasis
  * @typedef {('small' | 'medium' | 'large')} ButtonSize
+ */
+
+/**
+ * @typedef {import('@welingtonms/xb-toolset/dist/selection').SelectionType} ToggleGroupType
+ * @typedef {import('../../styles/size.styles').ElementSize} ToggleSize
+ */
+
+/**
+ * @typedef {Object} ToggleGroupAttributes
+ * @property {ToggleGroupType} type Strategy for toggling.
+ * @property {boolean} disabled Should the button be disabled.
+ * @property {ToggleSize} size Toggle button size.
+ * @property {'group' | 'radiogroup'} role Aria role
+ * @property {string | string[]} [value] Selection value
  */
