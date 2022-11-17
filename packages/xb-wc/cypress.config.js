6acfee6edb45eb4f9e1a3a44da2d015c73a60871
Welington Silva <welington.silva@loadsmart.com>
2022-11-16 21:21:24 -0300

    feat: upgrade to cypress 11

--- packages/xb-wc/cypress.config.js
+++ packages/xb-wc/cypress.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require( 'cypress' );
 
-export default defineConfig( {
+module.exports = defineConfig( {
 	component: {
 		devServer: {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore At this point we don't need any specific configuration, so a default webpack sourcing is enough
 			framework: undefined, // Source: https://github.com/cypress-io/cypress/blob/af415e044d39cedfb008541a609b02d8811931d3/npm/webpack-dev-server/src/devServer.ts#L132
 			bundler: 'webpack',
 		},
