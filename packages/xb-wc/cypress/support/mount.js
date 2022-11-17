2176cb3f5a17080b1e4335b6012067dddebc8e8d
Welington Silva <welington.silva@loadsmart.com>
2022-11-16 21:21:52 -0300

    refactor: simplify custom cy.mount

--- packages/xb-wc/cypress/support/mount.js
+++ packages/xb-wc/cypress/support/mount.js
@@ -1,44 +1,24 @@
-import {
-	injectStylesBeforeElement,
-	getContainerEl,
-} from '@cypress/mount-utils';
+import { getContainerEl } from '@cypress/mount-utils';
 import { render } from 'lit-html';
 
-function mount( Component, options = {} ) {
-	console.log( Component );
-	const componentName = 'xb-button';
+/**
+ * [Source](https://github.com/cypress-io/cypress/tree/develop/npm/mount-utils#readme)
+ * @param {import('lit').TemplateResult} webComponent
+ * @returns {Cypress.Chainable}
+ */
+function mount( webComponent ) {
+	const $root = getContainerEl();
 
-	const message = `<${ componentName } ... />`;
-	let logInstance;
+	// Render HTML containing component.
+	// https://lit.dev/docs/libraries/standalone-templates/#rendering-lit-html-templates
+	render( webComponent, $root );
 
-	return cy.then( () => {
-		if ( options.log !== false ) {
-			logInstance = Cypress.log( {
-				name: 'mount',
-				message: [ message ],
-			} );
-		}
-
-		const document = cy.state( 'document' );
-		const el = getContainerEl();
-		injectStylesBeforeElement( options, document, el );
-
-		render( Component, el );
-
-		return cy
-			.wrap( Component, { log: false } )
-			.wait( 1, { log: false } )
-			.then( () => {
-				if ( logInstance ) {
-					logInstance.snapshot( 'mounted' );
-					logInstance.end();
-				}
-
-				// by returning undefined we keep the previous subject
-				// which is the mounted component
-				return undefined;
-			} );
+	Cypress.log( {
+		name: 'mount',
+		message: [ `xb-element ... />` ],
 	} );
+
+	return cy.wrap( $root, { log: false } );
 }
 
 export default mount;
