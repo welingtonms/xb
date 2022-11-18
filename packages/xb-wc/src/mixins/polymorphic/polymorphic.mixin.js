34d55e425ca08cfc7c9cd88427da18aff2bb413c
Welington Silva <welington.silva@loadsmart.com>
2022-11-18 18:30:33 -0300

    feat: add polymorphic mixin
    This mixin allows an element to have an `as` attribute that works similarly to the
    [`as` prop, from styled-components](https://styled-components.com/docs/api#as-polymorphic-prop).

---
+++ packages/xb-wc/src/mixins/polymorphic/polymorphic.mixin.js
@@ -0,0 +1,49 @@
+import { literal, unsafeStatic } from 'lit/static-html.js';
+
+/**
+ * Allow a component to be rendered as any HTML tag through the attribute `as`..
+ * @mixin
+ * @param {XBElement} superClass
+ * @returns {PolymorphicElement}
+ */
+const PolymorphicElementMixin = ( superClass ) =>
+	/**
+	 * @class
+	 */
+	class PolymorphicElement extends superClass {
+		static get properties() {
+			return {
+				/**
+				 * Which HTML tag should be used to render this element..
+				 * @type {HTMLTag}
+				 * @public
+				 */
+				as: { type: String },
+			};
+		}
+
+		constructor() {
+			super();
+
+			/**
+			 * @type {HTMLTag}
+			 **/
+			this.as = 'span';
+		}
+
+		getTag() {
+			return literal`${ unsafeStatic( this.as ) }`;
+		}
+	};
+
+export default PolymorphicElementMixin;
+
+/**
+ * @typedef {import('../../common/prop-types').HTMLTag} HTMLTag
+ * @typedef {import('../../common/xb-element').default} XBElement
+ */
+
+/**
+ * @typedef {Object} PolymorphicElement
+ * @property {HTMLTag} as
+ */
