368f7279a8a6132f89c426fe172604cc6463384e
Welington Silva <welington.silva@loadsmart.com>
2022-11-30 21:59:26 -0300

    feat: remove color token utility

--- packages/xb-wc/src/utils/get-token.js
+++ packages/xb-wc/src/utils/get-token.js
@@ -2,11 +2,19 @@ import { unsafeCSS } from 'lit';
 import { toCSSValue } from '@welingtonms/xb-tokens';
 
 /**
- * Get token as lit's `CSSResult`
+ * Get token as lit's `CSSResult`. Handles color tokens wrapping
+ * the token value in the `rgb` format, optionally, adding the `opacity`.
  * @param {Token} token - Miranda token
+ * @param {number} [alpha] - opacity (applicable only to color tokens)
  * @returns {CSSResult}
  */
-function getToken( token ) {
+function getToken( token, alpha ) {
+	token = String( token || '' );
+
+	if ( token.startsWith( 'color-' ) ) {
+		return unsafeCSS( toCSSValue( token, alpha ) );
+	}
+
 	return unsafeCSS( toCSSValue( token ) );
 }
 
