4e58a089893b5d6f22528901222901992b6bc791
Welington Silva <welington.manoels@gmail.com>
2022-12-18 11:20:32 -0300

    fix: query focus trap

--- packages/xb-wc/src/components/dropdown/dropdown.js
+++ packages/xb-wc/src/components/dropdown/dropdown.js
@@ -91,7 +91,7 @@ export class Dropdown extends XBElement {
 			trigger.open = this.open;
 
 			if ( this.open ) {
-				this._getFocusTrap().activate();
+				this._getFocusTrap()?.activate();
 			} else {
 				this._getFocusTrap()?.deactivate();
 				this._getTrigger().focus();
@@ -153,12 +153,7 @@ export class Dropdown extends XBElement {
 
 	_getFocusTrap() {
 		if ( this._trap == null ) {
-			/**
-			 * FIXME: find a better way to query the focus-trap element.
-			 * For some reason, querySelector('xb-focus-trap') in the menu
-			 * Shadow DOM is not working.
-			 */
-			[ this._trap ] = [ ...this._getMenu().shadowRoot.children ];
+			this._trap = this._getMenu().shadowRoot.querySelector( 'xb-focus-trap' );
 		}
 
 		return this._trap;
