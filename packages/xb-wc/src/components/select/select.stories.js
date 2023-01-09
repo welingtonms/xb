407eaa17dd4a651bb8eacdc10b3446abe51c700a
Welington Silva <welington.manoels@gmail.com>
2023-01-09 18:39:20 -0300

    docs: add select stories

--- packages/xb-wc/src/components/select/select.stories.js
+++ packages/xb-wc/src/components/select/select.stories.js
@@ -1,6 +1,7 @@
 import { html } from 'lit';
 
 import { PlacementArg, SizeArg } from '../../common/arg-types';
+import { useSyncFruits } from './select.fixtures';
 import './select';
 
 export default {
@@ -33,27 +34,77 @@ export default {
 	},
 };
 
-export const Playground = ( args ) => html`<xb-select
-	?loading=${ args.loading }
-	?multiple=${ args.multiple }
-	@xb-change=${ args.change }
->
-	<xb-option value="change">
-		<!-- <xb-icon name="favorite" slot="leading"></xb-icon> -->
-		Change</xb-option
+export const Playground = ( args ) => html`
+	<xb-select
+		?loading=${ args.loading }
+		?multiple=${ args.multiple }
+		@xb-change=${ args.change }
 	>
-	<xb-option value="accept">
-		<!-- <xb-icon name="star" slot="leading"></xb-icon> -->
-		Accept</xb-option
-	>
-	<xb-option value="leave">
-		<!-- <xb-icon name="cloud" slot="leading"></xb-icon> -->
-		Leave</xb-option
-	>
-</xb-select>`;
+		<xb-option value="change">
+			<!-- <xb-icon name="favorite" slot="leading"></xb-icon> -->
+			Change
+		</xb-option>
+		<xb-option value="accept">
+			<!-- <xb-icon name="star" slot="leading"></xb-icon> -->
+			Accept
+		</xb-option>
+		<xb-option value="leave">
+			<!-- <xb-icon name="cloud" slot="leading"></xb-icon> -->
+			Leave
+		</xb-option>
+	</xb-select>
+`;
 
 Playground.args = {
 	// placement: 'bottom-start',
 	loading: false,
+	multiple: false,
+};
+
+export const SyncDatasource = ( args ) => html`
+	<xb-select @xb-change=${ args.change } .datasources=${ [ useSyncFruits ] }>
+		<xb-option value="fixed-fruit">Fixed fruit</xb-option>
+	</xb-select>
+`;
+
+SyncDatasource.args = {
+	multiple: false,
+};
+
+export const AsyncDatasource = ( args ) => html`
+	<xb-select @xb-change=${ args.change }></xb-select>
+
+	<script>
+		function useAsyncUsers() {
+			return {
+				name: 'users',
+				adapter: {
+					getID( user ) {
+						return user.id;
+					},
+					getLabel( user ) {
+						return user.name;
+					},
+				},
+				fetch: async ( { regex, query } ) => {
+					const response = await fetch(
+						'https://gorest.co.in/public/v2/users',
+						{
+							method: 'GET',
+						}
+					);
+					const users = await response.json();
+
+					return users.filter( ( { name } ) => regex.test( name ) );
+				},
+			};
+		}
+
+		const select = document.querySelector( 'xb-select' );
+		select.datasources = [ useAsyncUsers ];
+	</script>
+`;
+
+AsyncDatasource.args = {
 	multiple: false,
 };
