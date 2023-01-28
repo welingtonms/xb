6f2d3b0b0e3bf305b412ab52350430d103ca12ce
Welington Silva <welington.manoels@gmail.com>
2023-01-28 15:21:56 -0300

    feat: upgrade to storybook 7 beta

--- packages/xb-wc/.storybook/main.js
+++ packages/xb-wc/.storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-	core: {
-		builder: 'webpack5',
-	},
 	stories: [
 		'../src/**/*.stories.mdx',
 		'../src/**/*.stories.@(js|jsx|ts|tsx)',
 	],
 	addons: [ '@storybook/addon-links', '@storybook/addon-essentials' ],
-	framework: '@storybook/web-components',
+	framework: {
+		name: '@storybook/web-components-webpack5',
+		options: {},
+	},
+	docs: {
+		autodocs: true,
+	},
 };
