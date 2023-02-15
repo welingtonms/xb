cccadf9554349227bf017eb0bafa43ccfc6c2e04
Welington Silva <welington.manoels@gmail.com>
2023-02-15 20:37:19 -0300

    feat: setup decorators

--- packages/xb-wc/rollup.config.js
+++ packages/xb-wc/rollup.config.js
@@ -1,3 +1,4 @@
+import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
@@ -23,6 +24,7 @@ export default {
 		}
 	},
 	plugins: [
+		babel( { babelHelpers: 'bundled' } ),
 		replace( { 'Reflect.decorate': 'undefined', preventAssignment: true } ),
 		resolve(),
 		commonjs(),
