832b71de73f08dcc3736c45f486bda67a2994bcc
Welington Silva <welington.manoels@gmail.com>
2023-04-14 10:24:40 -0300

    test: add cleanup function

--- packages/xb-wc/cypress/support/mount.js
+++ packages/xb-wc/cypress/support/mount.js
@@ -1,24 +1,37 @@
 import { getContainerEl } from '@cypress/mount-utils';
 import { render } from 'lit-html';
 
+const M_ROOT = '__cy_m_root';
+
+const cleanup = () => {
+	Cypress.$( `#${ M_ROOT }` ).remove();
+};
+
 /**
  * [Source](https://github.com/cypress-io/cypress/tree/develop/npm/mount-utils#readme)
  * @param {import('lit').TemplateResult} webComponent
  * @returns {Cypress.Chainable}
  */
 function mount( webComponent ) {
+	cleanup();
+
 	const $root = getContainerEl();
+
+	const componentNode = document.createElement( 'div' );
+	componentNode.id = M_ROOT;
+
+	$root.append( componentNode );
 
 	// Render HTML containing component.
 	// https://lit.dev/docs/libraries/standalone-templates/#rendering-lit-html-templates
-	render( webComponent, $root );
+	render( webComponent, componentNode );
 
 	Cypress.log( {
 		name: 'mount',
 		message: [ `xb-element ... />` ],
 	} );
 
-	return cy.wrap( $root, { log: false } );
+	return cy.wrap( componentNode, { log: false } );
 }
 
 export default mount;
