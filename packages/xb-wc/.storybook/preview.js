cb17f7f16979090237843c9649e46d6aabe2a90d
Welington Silva <welington.manoels@gmail.com>
2023-04-29 11:31:44 -0300

    docs: change preview background

--- packages/xb-wc/.storybook/preview.js
+++ packages/xb-wc/.storybook/preview.js
@@ -30,7 +30,7 @@ export const parameters = {
 export const decorators = [
 	( Story ) => html`
 		<div
-			style="display: flex; align-items: center; justify-content: center; min-height: 50vh; gap: var(--xb-spacing-4); padding: var(--xb-spacing-8); background-color: var(--xb-color-background); background-size: 10px 10px; background-image: repeating-linear-gradient(45deg, rgba(var(--xb-color-gray-200), 0.75) 0, rgba(var(--xb-color-gray-200), 0.75) 1px, rgb(var(--xb-color-background)) 0, rgb(var(--xb-color-background)) 50%);"
+			style="display: flex; align-items: center; justify-content: center; min-height: 50vh; gap: var(--xb-spacing-4); padding: var(--xb-spacing-8); background-color: var(--xb-color-background); background-size: 10px 10px; background-image: repeating-linear-gradient(45deg, rgba(var(--xb-color-gray-100), 0.9) 0, rgba(var(--xb-color-gray-100), 0.9) 1px, rgb(var(--xb-color-background)) 0, rgb(var(--xb-color-background)) 50%);"
 		>
 			${ Story() }
 		</div>
