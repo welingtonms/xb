069ad616e2a5fcc961460f2cfa87cd1b548f9757
Welington Silva <welington.manoels@gmail.com>
2023-07-29 11:23:06 -0300

    docs: add a11y storybook addon

--- packages/xb-wc/.storybook/main.js
+++ packages/xb-wc/.storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
 	stories: [ '../src/**/*.@(stories|api).mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)' ],
-	addons: [ '@storybook/addon-links', '@storybook/addon-essentials' ],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-a11y',
+	],
 	framework: {
 		name: '@storybook/web-components-webpack5',
 		options: {},
