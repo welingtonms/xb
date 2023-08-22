83ac3828c8db110176dbc8ac8c0ddad3082682cc
Welington Silva <welington.manoels@gmail.com>
2023-08-22 09:32:47 -0300

    feat: create individual logger instances

--- packages/xb-wc/src/utils/logger.js
+++ packages/xb-wc/src/utils/logger.js
@@ -11,20 +11,27 @@ function log( fn, ...args ) {
 	fn( ...args );
 }
 
-// Define a logger object that contains four methods: debug, info, warn, and error
-const logger = Object.freeze( {
-	debug: ( ...args ) => {
-		log( console.debug, ...args );
-	},
-	info: ( ...args ) => {
-		log( console.log, ...args );
-	},
-	warn: ( ...args ) => {
-		log( console.warn, ...args );
-	},
-	error: ( ...args ) => {
-		log( console.error, ...args );
-	},
-} );
+/**
+ * Creates a logger object that contains four levels of logging: debug, info, warn, and error.
+ * @param {string} [prefix] kebab-case prefix to be added to the start of all log messages
+ */
+function createLogger( prefix = 'xb' ) {
+	const logger = Object.freeze( {
+		debug: ( ...args ) => {
+			log( console.debug, `[${ prefix }]`, ...args );
+		},
+		info: ( ...args ) => {
+			log( console.log, `[${ prefix }]`, ...args );
+		},
+		warn: ( ...args ) => {
+			log( console.warn, `[${ prefix }]`, ...args );
+		},
+		error: ( ...args ) => {
+			log( console.error, `[${ prefix }]`, ...args );
+		},
+	} );
 
-export default logger;
+	return logger;
+}
+
+export default createLogger;
