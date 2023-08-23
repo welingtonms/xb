eecf1ab45a1830d9a53e9d0f56f7f68588eff04a
Welington Silva <welington.manoels@gmail.com>
2023-08-23 20:25:06 -0300

    fix: add index for radio-group controller

---
+++ packages/xb-wc/src/controllers/radio-group-pattern/index.js
@@ -0,0 +1,6 @@
+export { default } from './radio-group-pattern.controller';
+
+/**
+ * @typedef {import('./radio-group-pattern.controller').default} ListboxPatternController
+ * @typedef {import('./radio-group-pattern.controller').ListboxPatternControllerHost} ListboxPatternControllerHost
+ */
