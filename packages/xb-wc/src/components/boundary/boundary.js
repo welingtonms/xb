111dc9b7f4a66102b8e19518c6d12550b8f638bd
Welington Silva <welington.manoels@gmail.com>
2023-09-01 20:45:11 -0300

    feat: add boundary controller
    - split boundary check code to the controller
    - apply the controller to the boundary element
    
    BREAKING CHANGE: The boundary starts up deactivated (inactive) by default.
    It requires calling the `activate` method to activate it.

--- packages/xb-wc/src/components/boundary/boundary.js
+++ packages/xb-wc/src/components/boundary/boundary.js
@@ -1,9 +1,10 @@
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+import BoundaryController from '../../controllers/boundary';
 import XBElement from '../../common/xb-element';
+
 import styles from './boundary.styles';
-import Keyboard from '../../common/keyboard';
 
 @customElement( 'xb-boundary' )
 export class InteractionBoundary extends XBElement {
@@ -15,22 +16,30 @@ export class InteractionBoundary extends XBElement {
 	 */
 	@property( { type: Boolean, reflect: true } ) active;
 
+	/** @type {BoundaryController} */
+	_controller;
+
 	constructor() {
 		super();
 
 		this.active = false;
+
+		this._controller = new BoundaryController( this );
 	}
 
-	connectedCallback() {
-		super.connectedCallback();
+	/**
+	 * @param {import('lit').PropertyValues<this>} changedProperties
+	 */
+	update( changedProperties ) {
+		super.update( changedProperties );
 
-		this._subscribe();
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback();
-
-		this._unsubscribe();
+		if ( changedProperties.has( 'active' ) ) {
+			if ( this.active ) {
+				this._controller.activate();
+			} else {
+				this._controller.deactivate();
+			}
+		}
 	}
 
 	render() {
@@ -53,44 +62,5 @@ export class InteractionBoundary extends XBElement {
 		}
 
 		this.active = false;
-	};
-
-	_subscribe() {
-		document.addEventListener( 'mousedown', this._handleEvent );
-		document.addEventListener( 'keyup', this._handleEvent );
-		document.addEventListener( 'touchend', this._handleEvent );
-		window.addEventListener( 'blur', this._handleBlurEvent );
-	}
-
-	_unsubscribe() {
-		document.removeEventListener( 'mousedown', this._handleEvent );
-		document.removeEventListener( 'keyup', this._handleEvent );
-		document.removeEventListener( 'touchend', this._handleEvent );
-		window.removeEventListener( 'blur', this._handleBlurEvent );
-	}
-
-	_handleEvent = ( e ) => {
-		const isInside = e.composedPath().includes( this );
-
-		if ( isInside && ! this.active ) {
-			this.activate();
-			this.emit( 'xb:interact-in' );
-		} else if ( ( ! isInside || Keyboard( e ).is( 'ESC' ) ) && this.active ) {
-			this.deactivate();
-			this.emit( 'xb:interact-out' );
-		}
-	};
-
-	_handleBlurEvent = ( e ) => {
-		const isInside = e.composedPath().includes( this );
-
-		if ( ! isInside && this.active ) {
-			/**
-			 * if the blur event happened in the watched element and the click outside
-			 * watcher is activated, then we deactivate it
-			 */
-			this.deactivate();
-			this.emit( 'xb:interact-out' );
-		}
 	};
 }
