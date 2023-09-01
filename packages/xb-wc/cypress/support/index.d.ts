116429d14f74b44129c0527aaf10e98d8b0812f6
Welington Silva <welington.manoels@gmail.com>
2023-09-01 20:37:07 -0300

    test: add (key) press command

--- packages/xb-wc/cypress/support/index.d.ts
+++ packages/xb-wc/cypress/support/index.d.ts
@@ -5,11 +5,10 @@ declare namespace Cypress {
 		/**
 		 * **Simulates pressing a keyboard key.**
 		 *
-		 * @param key string - The key you want to keydown. Available keys are: selectAll, moveToStart, moveToEnd, del, backspace, esc, enter, rightArrow, leftArrow, upArrow, downArrow, home, end, insert, pageUp, pageDown, {, alt, option, ctrl, control, meta, command, cmd, shift, ctrl+a, CTRL+A, cmd+a, CMD+A
-		 *
-		 * @example cy.get('input[type="text"]').type('something').keydown('enter')
-		 * @example cy.get('input[type="text"]').type('yoo').keydown('backspace').keydown('enter')
+		 * @param code - The key code you want to press.
 		 */
-		keydown( key: string ): Cypress.Chainable;
+		press(
+			code: 'Space' | 'Enter' | 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight'
+		): Cypress.Chainable;
 	}
 }
