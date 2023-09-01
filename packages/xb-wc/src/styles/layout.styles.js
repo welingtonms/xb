89e7975fb5ddd0e32485323ece18b2f32b34fe1c
Welington Silva <welington.manoels@gmail.com>
2023-09-01 20:32:18 -0300

    feat: add host to layout styles

--- packages/xb-wc/src/styles/layout.styles.js
+++ packages/xb-wc/src/styles/layout.styles.js
@@ -3,35 +3,43 @@ import { css } from 'lit';
 function styles() {
 	return [
 		css`
-			.-no-t-border {
+			.-no-t-border,
+			:host( :is( [borderless*='top'], [borderless*='vertical'] ) ) {
 				border-top: none !important;
 			}
 
-			.-no-r-border {
+			.-no-r-border,
+			:host( :is( [borderless*='right'], [borderless*='horizontal'] ) ) {
 				border-right: none !important;
 			}
 
-			.-no-b-border {
+			.-no-b-border,
+			:host( :is( [borderless*='bottom'], [borderless*='vertical'] ) ) {
 				border-bottom: none !important;
 			}
 
-			.-no-l-border {
+			.-no-l-border,
+			:host( :is( [borderless*='left'], [borderless*='horizontal'] ) ) {
 				border-left: none !important;
 			}
 
-			.-no-t-padding {
+			.-no-t-padding,
+			:host( :is( [paddingless*='top'], [paddingless*='vertical'] ) ) {
 				padding-top: 0 !important;
 			}
 
-			.-no-r-padding {
+			.-no-r-padding,
+			:host( :is( [paddingless*='right'], [paddingless*='horizontal'] ) ) {
 				padding-right: 0 !important;
 			}
 
-			.-no-b-padding {
+			.-no-b-padding,
+			:host( :is( [paddingless*='bottom'], [paddingless*='vertical'] ) ) {
 				padding-bottom: 0 !important;
 			}
 
-			.-no-l-padding {
+			.-no-l-padding,
+			:host( :is( [paddingless*='left'], [paddingless*='horizontal'] ) ) {
 				padding-left: 0 !important;
 			}
 		`,
