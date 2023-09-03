584b4f3daebd9c10ddae1fa2632b5b644876d53c
Welington Silva <welington.manoels@gmail.com>
2023-09-03 13:48:41 -0300

    chore: fix as-link mixin jsdoc

--- packages/xb-wc/src/common/prop-types.js
+++ packages/xb-wc/src/common/prop-types.js
@@ -30,6 +30,7 @@
  */
 
 /**
+ * Based on https://www.typescriptlang.org/play?ssl=8&ssc=4&pln=5&pc=1&filetype=js#code/PQKhCgAIUgBAXAngBwKYBNUDNIG8B2qA7pABQB0lAhgE4DmAzgFyRX6IDaAugJSQC8APjwBfEZADCAe3wN4NAK4BjeFQBGAG1RQQwcOFARoceKgC2yDVVN5ps+ctWbU4gCo64tOpABCVBqh4riI6elgK+CoAljKQAOKo+Jg0ALJRAB5R+KR+ATy4UJBFNKjwCjT4kEpWDAzxicmQqOmmSXW5qAVF3d10pQlJqDSk+SE9ISGGHgjmltaBuHZyiirqWm7TXr7+C8Gh4OGR8DGVAHJUZqhpmdkd+YXFpeWV1f5155dNLQ3tO109vVKH1QI1wY26EwMYGmpgsVhsixky0caxckHcxlgWw6QTGugOEWisQAgn1rlkcjt7j0SmUKlUanVSYFmq10L8Av8AZA+vBmaDwUVIeBXrVIABlMqYfDwL5splkjIU4Hk7IDZKq0iiupgnh6rk80qQcAiIA
  * @typedef {new (...args: any[]) => XBElement} Constructor
  */
 
