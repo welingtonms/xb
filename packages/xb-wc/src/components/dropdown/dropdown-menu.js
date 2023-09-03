e95ee97292b87d041dc0d9be5fd36b8168c69071
Welington Silva <welington.manoels@gmail.com>
2023-09-03 13:48:33 -0300

    feat: add dropdown controller
    This commit concentrates the keyboard interface for the dropdown
    trigger and menu in the same controller.
    
    This was based in the lit.dev Events page, which recommends this approach
    as a way to reduce code and improve performance. It says:
    
    > Using event delegation can reduce the number of event listeners used and
    > therefore improve performance. It is also sometimes convenient to centralize
    > event handling to reduce code.

--- packages/xb-wc/src/components/dropdown/dropdown-menu.js
+++ packages/xb-wc/src/components/dropdown/dropdown-menu.js
@@ -1,12 +1,13 @@
 import { customElement } from 'lit/decorators.js';
 
-import { Menu } from '../menu';
+import { BaseMenu } from '../menu';
 
 @customElement( 'xb-dropdown-menu' )
-export class DropdownMenu extends Menu {
+export class DropdownMenu extends BaseMenu {
 	connectedCallback() {
 		super.connectedCallback();
 
+		this.setAttribute( 'role', 'menu' );
 		this.setAttribute( 'tabindex', 0 );
 	}
 }
