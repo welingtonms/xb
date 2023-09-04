5090db66c64b1be0d15da30cee17b4e9f57a19df
Welington Silva <welington.manoels@gmail.com>
2023-09-04 13:54:14 -0300

    test: assert typed item search

--- packages/xb-wc/src/components/menu/menu.cy.js
+++ packages/xb-wc/src/components/menu/menu.cy.js
@@ -10,29 +10,41 @@ describe( '<xb-menu>', () => {
 		cy.mount(
 			html`
 				<xb-menu>
-					<xb-item data-cy="accept">Accept</xb-item>
-					<xb-item data-cy="change" @click=${ ( e ) => onClickSpy( e ) }>Change</xb-item>
-					<xb-item data-cy="leave">Leave</xb-item>
+					<xb-item id="item-accept">Accept</xb-item>
+					<xb-item id="item-change" @click=${ ( e ) => onClickSpy( e ) }>Change</xb-item>
+					<xb-item id="item-leave">Leave</xb-item>
 				</xb-menu>
 			`
 		);
 
-		cy.get( 'xb-menu' ).should( 'have.attr', 'role', 'menu' );
+		cy.get( 'xb-menu' ).as( 'menu' );
 
-		cy.get( 'xb-item[data-cy="accept"]' ).should( 'have.attr', 'role', 'menuitem' );
-		cy.get( 'xb-item[data-cy="change"]' ).should( 'have.attr', 'role', 'menuitem' );
-		cy.get( 'xb-item[data-cy="leave"]' ).should( 'have.attr', 'role', 'menuitem' );
+		cy.get( '@menu' ).should( 'have.attr', 'role', 'menu' );
+		cy.get( '@menu' ).should( 'have.attr', 'tabindex', '0' );
+		cy.get( '@menu' ).should( 'not.have.attr', 'aria-activedescendant' );
 
-		cy.get( 'xb-item[data-cy="change"]' ).click();
+		cy.get( 'xb-item#item-accept' )
+			.should( 'have.attr', 'role', 'menuitem' )
+			.and( 'have.attr', 'tabindex', '-1' );
+		cy.get( 'xb-item#item-change' )
+			.should( 'have.attr', 'role', 'menuitem' )
+			.and( 'have.attr', 'tabindex', '-1' );
+		cy.get( 'xb-item#item-leave' )
+			.should( 'have.attr', 'role', 'menuitem' )
+			.and( 'have.attr', 'tabindex', '-1' );
+
+		cy.get( 'xb-item#item-change' ).click();
 		cy.get( '@onClickSpy' ).should( 'have.been.called' );
 	} );
 
 	it( 'should navigate the keyboard', () => {
+		const onClickSpy = cy.stub().as( 'onClickSpy' );
+
 		cy.mount( html`
 			<xb-menu>
-				<xb-item data-cy="accept">Accept</xb-item>
-				<xb-item data-cy="change" @click=${ ( e ) => onClickSpy( e ) }>Change</xb-item>
-				<xb-item data-cy="leave">Leave</xb-item>
+				<xb-item id="item-accept">Accept</xb-item>
+				<xb-item id="item-change" @click=${ ( e ) => onClickSpy( e ) }>Change</xb-item>
+				<xb-item id="item-leave">Leave</xb-item>
 			</xb-menu>
 		` );
 
@@ -41,19 +53,55 @@ describe( '<xb-menu>', () => {
 		cy.get( '@menu' ).focus();
 
 		// navigates from first option to last option
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-accept' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Accept' );
+
 		cy.get( '@menu' ).press( 'ArrowDown' );
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-change' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Change' );
+
 		cy.get( '@menu' ).press( 'ArrowDown' );
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-leave' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Leave' );
+
 		cy.get( '@menu' ).press( 'ArrowDown' ); // return to first option
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-accept' );
 
 		// // navigates from last option to first option
 		cy.get( '@menu' ).press( 'ArrowUp' );
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-leave' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Leave' );
+
 		cy.get( '@menu' ).press( 'ArrowUp' );
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-change' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Change' );
+
 		cy.get( '@menu' ).press( 'ArrowUp' );
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-accept' );
 		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Accept' );
+	} );
+
+	it( 'should focus via keyboard-typed search', () => {
+		// this is powed by the focus-manager
+
+		cy.mount( html`
+			<xb-menu>
+				<xb-item id="item-accept">Accept</xb-item>
+				<xb-item id="item-change">Change</xb-item>
+				<xb-item id="item-leave">Leave</xb-item>
+			</xb-menu>
+		` );
+
+		cy.get( 'xb-menu' ).as( 'menu' );
+
+		cy.get( '@menu' ).focus();
+
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-accept' );
+		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Accept' );
+
+		cy.get( '@menu' ).type( 'leav' );
+
+		cy.get( '@menu' ).should( 'have.attr', 'aria-activedescendant', 'item-leave' );
+		cy.get( '@menu' ).find( '.is-focused' ).should( 'have.text', 'Leave' );
 	} );
 } );
