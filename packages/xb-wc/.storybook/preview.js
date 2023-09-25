26116ae378753f3de65bf1d26ee6a3300fa372c3
Welington Silva <welington.manoels@gmail.com>
2023-09-25 09:51:03 -0300

    feat: upgrade storybook

--- packages/xb-wc/.storybook/preview.js
+++ packages/xb-wc/.storybook/preview.js
@@ -3,33 +3,37 @@ import { html } from 'lit-html';
 import '@welingtonms/xb-tokens/dist/tokens/web/xb/variables.css';
 import './preview.css';
 
-export const parameters = {
-	actions: { argTypesRegex: '^on[A-Z].*' },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
+/** @type {import('@storybook/web-components').Preview} */
+const preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+		layout: 'fullscreen',
+		options: {
+			storySort: {
+				order: [
+					'Introduction',
+					'Getting started',
+					'Design Tokens',
+					'Contributing',
+					'Foundation',
+					'Components',
+					'Metacomponents',
+					'Type definitions',
+				],
+			},
 		},
 	},
-	layout: 'fullscreen',
-	options: {
-		storySort: {
-			order: [
-				'Introduction',
-				'Getting started',
-				'Design Tokens',
-				'Contributing',
-				'Foundation',
-				'Components',
-				'Metacomponents',
-				'Type definitions',
-			],
-		},
-	},
+	decorators: [
+		( Story ) => html`
+			<div id="xb-root">${ Story() }</div>
+		`,
+	],
 };
 
-export const decorators = [
-	( Story ) => html`
-		<div id="xb-root">${ Story() }</div>
-	`,
-];
+export default preview;
