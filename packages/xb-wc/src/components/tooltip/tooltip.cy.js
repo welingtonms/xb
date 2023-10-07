38743be81dca69335fdbe9d7e2350c98a98140d0
Welington Silva <welington.manoels@gmail.com>
2023-10-07 12:31:13 -0300

    feat: use tooltip anchor by id
    This approach is based on Fast DS tooltip component.
    
    Instead of wrapping the reference element, now we get its ID and use it
    to position the floating element (now, the m-tooltip itself).
    
    For that reason, the tooltip styles had to absorb the floating styles, thus,
    we no longer import it.

--- packages/xb-wc/src/components/tooltip/tooltip.cy.js
+++ packages/xb-wc/src/components/tooltip/tooltip.cy.js
@@ -5,71 +5,75 @@ import '../layout/box';
 
 describe( '<xb-tooltip>', () => {
 	it( 'should render on click', () => {
-		const args = { placement: 'bottom-start', trigger: 'click' };
+		/** @type {TooltipAttributes} */
+		const args = { trigger: [ 'click' ] };
 
 		cy.mount( html`
-			<xb-tooltip placement=${ args.placement } trigger=${ args.trigger }>
-				<button type="button" slot="reference">reference</button>
-				<div slot="floating">Lorem ipsum dolor sit amet.</div>
+			<button id="my-anchor" type="button">reference</button>
+
+			<xb-tooltip anchor="my-anchor" .trigger=${ args.trigger }>
+				Lorem ipsum dolor sit amet.
 			</xb-tooltip>
 		` );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).click();
+		cy.get( '#my-anchor' ).click();
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'be.visible' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).click();
+		cy.get( '#my-anchor' ).click();
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 	} );
 
 	it( 'should render on hover', () => {
-		const args = { placement: 'bottom-start', trigger: 'hover' };
+		/** @type {TooltipAttributes} */
+		const args = { trigger: [ 'hover' ] };
 
 		cy.mount( html`
-			<xb-tooltip placement=${ args.placement } trigger=${ args.trigger }>
-				<button type="button" slot="reference">reference</button>
-				<div slot="floating">Lorem ipsum dolor sit amet.</div>
+			<button id="my-anchor" type="button">reference</button>
+
+			<xb-tooltip anchor="my-anchor" .trigger=${ args.trigger }>
+				Lorem ipsum dolor sit amet.
 			</xb-tooltip>
 		` );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).trigger( 'mouseover' );
+		cy.get( '#my-anchor' ).trigger( 'mouseover' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'be.visible' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).trigger( 'mouseout' );
+		cy.get( '#my-anchor' ).trigger( 'mouseout' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 	} );
 
 	it( 'should render on focus', () => {
-		const args = { placement: 'bottom-start', trigger: 'focus' };
+		/** @type {TooltipAttributes} */
+		const args = { trigger: [ 'focus' ] };
 
 		cy.mount( html`
-			<xb-tooltip placement=${ args.placement } trigger=${ args.trigger }>
-				<button type="button" slot="reference">reference</button>
-				<div slot="floating">Lorem ipsum dolor sit amet.</div>
+			<button id="my-anchor" type="button">reference</button>
+
+			<xb-tooltip anchor="my-anchor" .trigger=${ args.trigger }>
+				Lorem ipsum dolor sit amet.
 			</xb-tooltip>
 		` );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 
-		/**
-		 * no, this won't work (for headlesss, firefox, and electron):
-		 * cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).focus();
-		 * I tried it.
-		 * Tabbing to focus on first focusable element instead.
-		 */
-		cy.get( 'body' ).tab();
+		cy.get( '#my-anchor' ).focus();
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'be.visible' );
 
-		cy.get( 'xb-tooltip' ).find( '[slot="reference"]' ).blur();
+		cy.get( '#my-anchor' ).blur();
 
-		cy.get( 'xb-tooltip' ).find( '[slot="floating"]' ).should( 'not.be.visible' );
+		cy.get( 'xb-tooltip' ).should( 'not.be.visible' );
 	} );
 } );
+
+/**
+ * @typedef {import('./tooltip').TooltipAttributes} TooltipAttributes
+ */
