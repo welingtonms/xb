d7a0adbb07a7192b2f4e079cba2f24860fe67ccf
Welington Silva <welington.manoels@gmail.com>
2023-10-17 09:26:27 -0300

    feat: use popover API for the dropdown menu
    - use popover API for the dropdown menu, when supported
    - concentrate all dropdown styles in the dropdown.styles

--- packages/xb-wc/src/components/dropdown/dropdown.styles.js
+++ packages/xb-wc/src/components/dropdown/dropdown.styles.js
@@ -1,11 +1,67 @@
-import { floatingStyles } from '../../common/floating-element';
+import { css } from 'lit';
 
-function styles() {
+import { floatingStyles } from '../../common/floating-element';
+import token from '../../utils/get-token';
+import transition from '../../styles/transition.styles';
+import baseMenuStyles from '../menu/base-menu.styles';
+
+export function dropdownStyles() {
 	return [
 		floatingStyles( {
 			floating: '::slotted([role="menu"])',
 		} ),
+		css`
+			:host {
+				--xb-floating-width: max-content;
+				--xb-floating-min-width: 16ch;
+			}
+		`,
 	];
 }
 
-export default styles;
+export function menuStyles() {
+	return [ baseMenuStyles() ];
+}
+
+export function triggerStyles() {
+	return [
+		css`
+			:host {
+				--xb-button-padding-x: ${ token( 'spacing-4' ) };
+				--xb-button-background-color: ${ token( 'color-primary-500' ) };
+				--xb-button-border-color: ${ token( 'color-primary-500' ) };
+				--xb-button-color: ${ token( 'color-white' ) };
+			}
+
+			:host( :hover ),
+			:host( [aria-expanded='true'] ) {
+				--xb-button-background-color: ${ token( 'color-secondary-500' ) };
+				--xb-button-border-color: ${ token( 'color-secondary-500' ) };
+				--xb-button-color: ${ token( 'color-white' ) };
+			}
+
+			:host( :is( :active, .is-active ) ) {
+				--xb-button-background-color: ${ token( 'color-secondary-300' ) };
+				--xb-button-border-color: ${ token( 'color-secondary-300' ) };
+				--xb-button-color: ${ token( 'color-white' ) };
+				--xb-button-outline-color: ${ token( 'color-secondary-500', 0.2 ) };
+			}
+
+			:host( :is( :focus, :focus-within, :focus-visible, .is-focused ):hover ) {
+				--xb-button-outline-color: ${ token( 'color-secondary-500', 0.2 ) };
+			}
+
+			.indicator {
+				${ transition( [
+					{
+						property: 'transform',
+					},
+				] ) };
+			}
+
+			:host( [aria-expanded='true'] ) .indicator {
+				transform: rotate( 180deg );
+			}
+		`,
+	];
+}
