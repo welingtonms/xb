de35447fe98a8acba194df9dc9c157a82f29a6ea
Welington Silva <welington.manoels@gmail.com>
2023-10-17 09:37:10 -0300

    feat: improvements to spinner
    Remove unnecessary wrapper and use the host itself instead.

--- packages/xb-wc/src/components/spinner/spinner.js
+++ packages/xb-wc/src/components/spinner/spinner.js
@@ -1,7 +1,5 @@
 import { html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { createRef, ref } from 'lit/directives/ref.js';
-import withClassy from '@welingtonms/classy';
+import { customElement } from 'lit/decorators.js';
 
 import XBElement from '../../common/xb-element';
 import styles from './spinner.styles';
@@ -9,23 +7,10 @@ import styles from './spinner.styles';
 @customElement( 'xb-spinner' )
 export class Spinner extends XBElement {
 	static styles = [ styles() ];
-
-	/**
-	 * Loading message.
-	 * @type {SpinnerAttributes['message']}
-	 */
-	@property( { type: String } ) message;
-
-	element = createRef();
-
 	render() {
-		const { when, classy } = withClassy( {} );
-
 		return html`
-			<div ${ ref( this.element ) } class=${ classy( 'spinner', {} ) }>
-				<span class="bar"></span>
-				<slot></slot>
-			</div>
+			<span class="bar"></span>
+			<slot></slot>
 		`;
 	}
 
@@ -33,8 +18,3 @@ export class Spinner extends XBElement {
 		return this.element.value?.innerHTML === '';
 	}
 }
-
-/**
- * @typedef {Object} SpinnerAttributes
- * @property {string} [message] - Loading message.
- */
