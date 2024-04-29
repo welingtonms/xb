f92e8a36923674b76212643bb7c0c1c0cf135733
Welington Silva <welington.manoels@gmail.com>
2024-04-29 18:21:29 -0300

    chore: remove xb-toolset

--- packages/xb-toolset/src/selection/index.ts
+++
@@ -1,8 +0,0 @@
-export { default, SELECTION_TYPES } from './selection-strategy';
-
-export type {
-	SelectionStrategyCreationProps,
-	SelectionType,
-	SelectionState,
-	SelectionStrategy,
-} from './selection-strategy';
