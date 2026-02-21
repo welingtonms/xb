9651b57d64180c09b37998148fae54325dbaf0b5
Welington Silva <welington.manoels@gmail.com>
2026-02-21 18:34:14 -0300

    ci: enable release

--- .releaserc.js
+++ .releaserc.js
@@ -3,15 +3,7 @@ export default {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		[ '@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' } ],
 		[ '@semantic-release/npm', { npmPublish: true } ],
-		// [
-		// 	'@semantic-release/git',
-		// 	{
-		// 		assets: false,
-		// 		message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-		// 	},
-		// ],
 		'@semantic-release/github',
 	],
 };
