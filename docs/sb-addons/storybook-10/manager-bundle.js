51d8fe97862c1c0ab41bb4133bd86e11b4d68ada
Welington Silva <welington.manoels@gmail.com>
2026-06-05 20:12:40 -0300

    chore: drop the old static Storybook

--- docs/sb-addons/storybook-10/manager-bundle.js
+++
@@ -1,3 +0,0 @@
-try{
-var p=__STORYBOOKADDONS__,{addons:O,types:S,mockChannel:f}=__STORYBOOKADDONS__;O.setConfig({enableShortcuts:!1});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
